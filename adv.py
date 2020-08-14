230de66813aa5cfba5ad204f0e685f22730a1b1e
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-08-14 09:29:10 -0700

    improved queue to get number of steps slightly lower and more consistent

--- adv.py
+++ adv.py
@@ -15,8 +15,8 @@ world = World()
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+map_file = "maps/test_loop_fork.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -30,7 +30,9 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
-
+#  a faster solution might be:
+# for each node in the queue 
+# first find the paths that lead to dead ends first
 # create a dictionary of rooms and all the rooms that room is connected to
 rooms = {}
 for r in world.rooms:
@@ -61,6 +63,13 @@ for c in connections:
     rg.add_edge(c[0], c[1])
 # print(rg.vertices)
 
+def find_components(graph):
+    # traverse the graph
+    # when you get to a dead end
+    # create a path from the last node with open paths to the dead end
+    pass
+
+
 # what is the starting room?
 start_room = world.starting_room
 opposites = {
@@ -78,9 +87,9 @@ def get_path(starting_room):
     cur_room = starting_room
     open_paths = {}
     queue = []
+    shortest_path = []
     # queue.remove(cur_room.name)
     while len(visited) != total:
-        shortest_path = []
         
         dirs = cur_room.get_exits()
         open_dirs = []       
@@ -91,9 +100,21 @@ def get_path(starting_room):
         for d in dirs:
             # print(cur_room.get_room_in_direction(d).name)
             rname = cur_room.get_room_in_direction(d).name
-            if rname not in visited and rname not in queue:
+            if rname not in visited:
                 open_dirs.append(d)
-                queue.append(rname)
+        # if len(shortest_path) > 0:
+        #     if cur_room.name == shortest_path[-1]:
+        #         shortest_path = []
+        #         for d in open_dirs:
+        #             rname = cur_room.get_room_in_direction(d).name
+        #             if rname in queue:
+        #                 open_dirs.remove(d)
+        if len(open_dirs) > 0 and cur_room.name not in queue:
+            queue.append(cur_room.name)
+        for d in open_dirs:
+            rname = cur_room.get_room_in_direction(d).name
+            if rname in queue:
+                open_dirs.remove(d)
         if len(open_dirs) == 0 and cur_room.name in queue:
             for q in queue:
                 if cur_room.name == q:
@@ -105,6 +126,7 @@ def get_path(starting_room):
             # add that to the traversal path
             traversal_path.append(open_dirs[ran_dir])
             cur_room = player.current_room
+            open_paths[cur_room.name] = open_dirs
         if len(open_dirs) == 0:
             if len(queue) > 0:
                 shortest_path = rg.bfs(cur_room.name, queue[-1])
@@ -127,12 +149,13 @@ def get_path(starting_room):
             for d in spath_dirs:
                 player.travel(d)
                 traversal_path.append(d)
-                temp_room = player.current_room
-                temp_dirs = temp_room.get_exits()
-                for t in temp_dirs:
-                    rname = temp_room.get_room_in_direction(t).name
-                    if rname not in visited and rname not in queue:
-                        queue.append(rname)
+                # temp_room = player.current_room
+                # temp_dirs = temp_room.get_exits()
+                # for t in temp_dirs:
+                #     rname = temp_room.get_room_in_direction(t).name
+                #     if rname not in visited and rname not in queue:
+                #         queue.append(rname)
+        
             cur_room = player.current_room
             
 
